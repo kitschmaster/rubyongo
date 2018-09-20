5d6e6a6cb5841588dd168216bcab2ba09b8c5f34
mihael <kitschmaster@gmail.com>
2018-09-20 15:41:34 +0200

    Cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,30 +2,13 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'yaml'
 require 'minitest/autorun'
 require 'minitest/capybara'
+require 'rack/test'
 require "capybara"
 require "capybara/dsl"
-require 'rack/test'
-#require 'selenium-webdriver'
-
+require 'capybara/poltergeist'
 require 'rubyongo'
 require 'rubyongo/panel/test'
 
-require 'capybara'
-require 'capybara/dsl'
-require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
-
-# Unfortunately this is required to get js tests running, FF 48 works, but not the latest one.
-#begin
-#  Selenium::WebDriver::Firefox::Binary.path= File.expand_path "~/opensource/Firefox.app/Contents/MacOS/firefox-bin"
-#rescue
-#end
-
 Capybara.app = Rubyongo::Kit
-#Capybara.server_host = '0.0.0.0'
-#Capybara.server_port = 9393
-#Capybara.register_driver :selenium do |app|
-#  Capybara::Selenium::Driver.new(app, :browser => :firefox)
-#end
-#Capybara.default_driver = :selenium
 
