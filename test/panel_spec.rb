92eb98131d8628f823ade2fdc744c36f97eaa769
mihael <kitschmaster@gmail.com>
2018-09-20 15:20:01 +0200

    Switch to poltergeist

--- test/panel_spec.rb
+++ test/panel_spec.rb
@@ -4,7 +4,7 @@ class PanelSpec < Rubyongo::Spec
 
   it "works" do
     visit "/panel"
-    page.must_have_content 'GURU PANEL'
+    page.must_have_content 'Guru Panel'
     page.must_have_content 'Tune in'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,24 +5,27 @@ require 'minitest/capybara'
 require "capybara"
 require "capybara/dsl"
 require 'rack/test'
-require 'selenium-webdriver'
+#require 'selenium-webdriver'
 
 require 'rubyongo'
 require 'rubyongo/panel/test'
 
 require 'capybara'
 require 'capybara/dsl'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 # Unfortunately this is required to get js tests running, FF 48 works, but not the latest one.
-begin
-  Selenium::WebDriver::Firefox::Binary.path= File.expand_path "~/opensource/Firefox.app/Contents/MacOS/firefox-bin"
-rescue
-end
+#begin
+#  Selenium::WebDriver::Firefox::Binary.path= File.expand_path "~/opensource/Firefox.app/Contents/MacOS/firefox-bin"
+#rescue
+#end
 
 Capybara.app = Rubyongo::Kit
 #Capybara.server_host = '0.0.0.0'
 #Capybara.server_port = 9393
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
-end
-Capybara.default_driver = :selenium
+#Capybara.register_driver :selenium do |app|
+#  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+#end
+#Capybara.default_driver = :selenium
+
