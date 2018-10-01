492bccdc128a64a6b1841e8ca8b26d7870ea0c79
mihael <kitschmaster@gmail.com>
2018-10-01 22:58:08 +0200

    Tune in/out test

--- test/panel_spec.rb
+++ test/panel_spec.rb
@@ -8,14 +8,28 @@ class PanelSpec < Rubyongo::Spec
     page.must_have_content 'Tune in'
   end
 
-  it "tunes a guru in" do
-    visit "/panel"
-    click_on 'Tune in'
-    page.must_have_content 'Name'
-    page.must_have_content 'Pass'
-    fill_in 'guru[username]', :with => 'rubyongo'
-    fill_in 'guru[password]', :with => '3shop6shop9'
-    find('input[name="commit"]').click
-    page.must_have_content 'Successfully tuned in'
+  it "tunes a guru in and out" do
+    puts "what is app: #{page.driver.methods.sort}"
+    tunein
+    tuneout
   end
+
+  private
+
+    def tunein
+      visit "/panel"
+
+      click_on 'Tune in'
+      page.must_have_content 'Name'
+      page.must_have_content 'Pass'
+      fill_in 'guru[username]', :with => 'rubyongo'
+      fill_in 'guru[password]', :with => '3shop6shop9'
+      find('input[name="commit"]').click
+      page.must_have_content 'Successfully tuned in'
+    end
+
+    def tuneout
+      click_on 'Drop out'
+      page.must_have_content 'Successfully tuned out'
+    end
 end
