68a13990789a499f66e9b405ff44e078dadac332
mihael <kitschmaster@gmail.com>
2018-10-08 23:02:04 +0200

    Fix for travis

--- test/content_editor_spec.rb
+++ test/content_editor_spec.rb
@@ -24,7 +24,7 @@ class ContentEditorSpec < Rubyongo::Spec
     page.driver.execute_script("$('form.upload-form').removeClass('hidden')")
 
     # then attaching and upload begins automatically
-    attach_file('files[]', './public/img/rog_logo.png', visible: false)
+    attach_file('files[]', './static/img/rog_logo.png', visible: false)
 
     page.must_have_content "Uploaded"
     # checking for the filenames appear in the jsTree seems to result in intermittent failures
