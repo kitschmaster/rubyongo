1d8edb00908a5676a5abe1754ed842bccca4f6aa
mihael <kitschmaster@gmail.com>
2018-10-08 10:41:01 +0200

    Remove private methods, those are now in spec_helpers

--- test/panel_spec.rb
+++ test/panel_spec.rb
@@ -4,7 +4,7 @@ class PanelSpec < Rubyongo::Spec
 
   it "works" do
     visit "/panel"
-    page.must_have_content 'Guru Panel'
+    page.must_have_content 'Guru Panel'.upcase
     page.must_have_content 'Tune in'
   end
 
@@ -12,23 +12,4 @@ class PanelSpec < Rubyongo::Spec
     tunein
     tuneout
   end
-
-  private
-
-    def tunein
-      visit "/panel"
-
-      click_on 'Tune in'
-      page.must_have_content 'Name'
-      page.must_have_content 'Pass'
-      fill_in 'guru[username]', :with => 'rubyongo'
-      fill_in 'guru[password]', :with => '3shop6shop9'
-      find('button[type="submit"]').click
-      page.must_have_content 'Successfully tuned in'
-    end
-
-    def tuneout
-      click_on 'Drop out'
-      page.must_have_content 'Successfully tuned out'
-    end
 end
