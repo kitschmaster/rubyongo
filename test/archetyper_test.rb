9cc03bdb9755a738df2f414a36f02ad9afb04a7a
mihael <kitschmaster@gmail.com>
2018-10-11 16:10:11 +0200

    Add test

--- test/archetyper_test.rb
+++ test/archetyper_test.rb
@@ -2,8 +2,24 @@ require "test_helper"
 
 class ArchetyperTest < Minitest::Test
 
-  def test_archetyper_listing_archetypes
+  def test_listing_archetypes
     assert_equal Rubyongo::Archetyper.archetypes.sort, ["item", "page", "post", "project"].sort
   end
 
+  def test_create_with_image
+    image = ['example.png', 'example-thumb.png']
+
+    Rubyongo::Archetyper.create_with_image('./', 'item', image)
+
+    file = "./content/item/example.md"
+    file_exists = File.exists?(file)
+    assert_equal true, file_exists, "#{file} file should exist"
+
+    content = File.read(file)
+    assert_equal true, (content =~ /\(item\/example.png\)/) > 0, "#{file} should contain image tag"
+
+    if file_exists
+      FileUtils.rm_r(file)
+    end
+  end
 end
