83fc432d9bd03e4ff555dbf863403fad5bea0a63
mihael <kitschmaster@gmail.com>
2019-01-04 09:06:31 +0100

    Simple version reading/writing

--- lib/rubyongo/version.rb
+++ lib/rubyongo/version.rb
@@ -11,8 +11,19 @@ module Rubyongo
   #  STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   #end
 
+  # Gem version
   def self.version
     File.read(File.join(Rubyongo::GEM_PATH, 'ROG_VERSION'))
     #Rubyongo::VERSION::STRING
+  end
+
+  # Framework user's current version
+  def self.version_for(rog_dir)
+    File.read(File.join(rog_dir, 'VERSION'))
+  end
+
+  # Write the framework user's version during >rog new< and >rog upgrade<
+  def self.write_version_for(rog_dir, version)
+    File.write(File.join(rog_dir, 'VERSION'), version)
   end
 end
